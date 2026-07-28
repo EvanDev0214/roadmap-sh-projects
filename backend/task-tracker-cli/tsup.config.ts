4ef527d4fd044a467da07d49b45fe7164ec213e9
Evan <evan.dev.0214@gmail.com>
2026-07-28 10:27:25 +0800

    chore: add eslint config and script

--- backend/task-tracker-cli/tsup.config.ts
+++ backend/task-tracker-cli/tsup.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -6,5 +6,5 @@ export default defineConfig({
   target: 'es2023',
   splitting: false,
   sourcemap: false,
-  clean: true,
-})
+  clean: true
+});
